2760aa456d5879303c8d8e8bbc46cce6fe8f35af
Dks <54738768+DIPNEET@users.noreply.github.com>
2020-08-18 14:00:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen';
 
-export default function App() {
-  return (
-    <WelcomeScreen/>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <WelcomeScreen/>
+    );
+  }
+  
 }
